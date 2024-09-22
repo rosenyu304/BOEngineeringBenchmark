ba629aac4c461c32f9d279c397d1fe02b2d80f32
kailey-m-e <kaileyme@mit.edu>
2024-09-22 14:55:03 -0400

    Update PressureVessel.py

--- test_functions/PressureVessel.py
+++ test_functions/PressureVessel.py
@@ -11,9 +11,12 @@ import numpy as np
 #
 #
 
+
 def PressureVessel(individuals):
 
-    assert torch.is_tensor(individuals) and individuals.size(1) == 4, "Input must be an n-by-4 PyTorch tensor."
+    assert (
+        torch.is_tensor(individuals) and individuals.size(1) == 4
+    ), "Input must be an n-by-4 PyTorch tensor."
 
     C1, C2, C3, C4 = (0.6224, 1.7781, 3.1661, 19.84)
     fx, gx1, gx2, gx3, gx4 = (torch.tensor([]),) * 5
@@ -22,18 +25,20 @@ def PressureVessel(individuals):
 
     for i in range(n):
 
-        x = individuals[i,:]
+        x = individuals[i, :]
 
         Ts, Th, R, L = x
 
         ## Negative sign to make it a maximization problem
-        test_function = - ( C1*Ts*R*L + C2*Th*R*R + C3*Ts*Ts*L + C4*Ts*Ts*R )
+        test_function = -(
+            C1 * Ts * R * L + C2 * Th * R * R + C3 * Ts * Ts * L + C4 * Ts * Ts * R
+        )
         fx = torch.cat((fx, torch.tensor([[test_function]])))
 
-        gx1 = torch.cat((gx1, torch.tensor([[-Ts + 0.0193*R]])))
-        gx2 = torch.cat((gx2, torch.tensor([[-Th + 0.00954*R]])))
-        gx3 = torch.cat((gx3, torch.tensor([[(-1)*np.pi*R*R*L + (-1)*4/3*np.pi*R*R*R + 750*1728]])))
-        gx4 = torch.cat((gx4, torch.tensor([[L-240]])))
+        gx1 = torch.cat((gx1, torch.tensor([[-Ts + 0.0193 * R]])))
+        gx2 = torch.cat((gx2, torch.tensor([[-Th + 0.00954 * R]])))
+        gx3 = torch.cat((gx3, torch.tensor([[(-1) * np.pi * R * R * L + (-1) * 4 / 3 * np.pi * R * R * R + 750 * 1728]])))
+        gx4 = torch.cat((gx4, torch.tensor([[L - 240]])))
 
     gx = torch.cat((gx1, gx2, gx3, gx4), 1)
 
@@ -42,9 +47,11 @@ def PressureVessel(individuals):
 
 def PressureVessel_Scaling(X):
 
-    assert torch.is_tensor(X) and X.size(1) == 4, "Input must be an n-by-4 PyTorch tensor."
+    assert (
+        torch.is_tensor(X) and X.size(1) == 4
+    ), "Input must be an n-by-4 PyTorch tensor."
 
-    scale_mult = torch.tensor([(98*0.0625), (98*0.0625), (200-10), (200-10)])
+    scale_mult = torch.tensor([(98 * 0.0625), (98 * 0.0625), (200 - 10), (200 - 10)])
     scale_add = torch.tensor([0.0625, 0.0625, 10, 0])
 
     return torch.mul(X, scale_mult) + scale_add
