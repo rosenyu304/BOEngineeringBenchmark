2dc97423e8ee641134bf7322f5578fe373c893d4
kailey-m-e <kaileyme@mit.edu>
2024-09-22 14:53:35 -0400

    Update CompressionSpring.py

--- test_functions/CompressionSpring.py
+++ test_functions/CompressionSpring.py
@@ -11,9 +11,12 @@ import numpy as np
 #
 #
 
+
 def CompressionSpring(individuals):
 
-    assert torch.is_tensor(individuals) and individuals.size(1) == 3, "Input must be an n-by-3 PyTorch tensor."
+    assert (
+        torch.is_tensor(individuals) and individuals.size(1) == 3
+    ), "Input must be an n-by-3 PyTorch tensor."
 
     fx, gx1, gx2, gx3, gx4 = (torch.tensor([]),) * 5
 
@@ -21,23 +24,20 @@ def CompressionSpring(individuals):
 
     for i in range(n):
 
-        x = individuals[i,:]
+        x = individuals[i, :]
 
-        d = x[0]
-        D = x[1]
-        N = x[2]
+        d, D, N = x
 
         ## Negative sign to make it a maximization problem
-        test_function = - ( (N+2)*D*d**2 )
-        fx = torch.cat((fx, torch.tensor([test_function])))
+        test_function = -((N + 2) * D * d**2)
+        fx = torch.cat((fx, torch.tensor([[test_function]])))
 
-        ## Calculate and add the constraint terms
-        gx1 = torch.cat((gx1, torch.tensor([[1 -  ( D*D*D * N / (71785* d*d*d*d) )]])))
-        gx2 = torch.cat((gx2, torch.tensor([[(4*D*D - D*d) / (12566 * (D*d*d*d - d*d*d*d)) + 1/(5108*d*d) -  1]])))
-        gx3 = torch.cat((gx3, torch.tensor([[1 - 140.45*d / (D*D * N)]])))
-        gx4 = torch.cat((gx4, torch.tensor([[(D+d)/1.5 - 1]])))
+        ## Calculate and add constraint terms
+        gx1 = torch.cat((gx1, torch.tensor([[1 - (D * D * D * N / (71785 * d * d * d * d))]])))
+        gx2 = torch.cat((gx2, torch.tensor([[(4 * D * D - D * d) / (12566 * (D * d * d * d - d * d * d * d)) + 1 / (5108 * d * d) - 1]])))
+        gx3 = torch.cat((gx3, torch.tensor([[1 - 140.45 * d / (D * D * N)]])))
+        gx4 = torch.cat((gx4, torch.tensor([[(D + d) / 1.5 - 1]])))
 
-    fx = torch.reshape(fx, (len(fx),1))
     gx = torch.cat((gx1, gx2, gx3, gx4), 1)
 
     return gx, fx
@@ -45,9 +45,11 @@ def CompressionSpring(individuals):
 
 def CompressionSpring_Scaling(X):
 
-    assert torch.is_tensor(X) and X.size(1) == 3, "Input must be an n-by-3 PyTorch tensor."
+    assert (
+        torch.is_tensor(X) and X.size(1) == 3
+    ), "Input must be an n-by-3 PyTorch tensor."
 
-    scale_mult = torch.tensor([(1-0.05), (1.3-0.25), (15-2)])
+    scale_mult = torch.tensor([(1 - 0.05), (1.3 - 0.25), (15 - 2)])
     scale_add = torch.tensor([0.05, 0.25, 2])
 
     return torch.mul(X, scale_mult) + scale_add
