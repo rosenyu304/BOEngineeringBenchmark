f921d55e4e9853c697b1b45bb97d03e3afc76fb6
kailey-m-e <kaileyme@mit.edu>
2024-09-22 14:55:35 -0400

    Update ReinforcedConcreteBeam.py

--- test_functions/ReinforcedConcreteBeam.py
+++ test_functions/ReinforcedConcreteBeam.py
@@ -11,9 +11,12 @@ import numpy as np
 #
 #
 
+
 def ReinforcedConcreteBeam(individuals):
 
-    assert torch.is_tensor(individuals) and individuals.size(1) == 3, "Input must be an n-by-3 PyTorch tensor."
+    assert (
+        torch.is_tensor(individuals) and individuals.size(1) == 3
+    ), "Input must be an n-by-3 PyTorch tensor."
 
     fx, gx1, gx2, gx3, gx4 = (torch.tensor([]),) * 5
 
@@ -21,16 +24,15 @@ def ReinforcedConcreteBeam(individuals):
 
     for i in range(n):
 
-        x = individuals[i,:]
+        x = individuals[i, :]
 
         As, h, b = x
 
-        test_function = - ( 29.4*As + 0.6*b*h )
+        test_function = -(29.4 * As + 0.6 * b * h)
         fx = torch.cat((fx, torch.tensor([[test_function]])))
 
-        gx1 = torch.cat((gx1, torch.tensor([[h/b - 4]])))
-        gx2 = torch.cat((gx2, torch.tensor([[180 + 7.35*As*As/b - As*h]])))
-
+        gx1 = torch.cat((gx1, torch.tensor([[h / b - 4]])))
+        gx2 = torch.cat((gx2, torch.tensor([[180 + 7.35 * As * As / b - As * h]])))
 
     gx = torch.cat((gx1, gx2), 1)
 
@@ -39,9 +41,11 @@ def ReinforcedConcreteBeam(individuals):
 
 def ReinforcedConcreteBeam_Scaling(X):
 
-    assert torch.is_tensor(X) and X.size(1) == 3, "Input must be an n-by-3 PyTorch tensor."
+    assert (
+        torch.is_tensor(X) and X.size(1) == 3
+    ), "Input must be an n-by-3 PyTorch tensor."
 
-    scale_mult = torch.tensor([(15-0.2), (40-28), (5)])
+    scale_mult = torch.tensor([(15 - 0.2), (40 - 28), (5)])
     scale_add = torch.tensor([0.2, 28, 5])
 
     return torch.mul(X, scale_mult) + scale_add
